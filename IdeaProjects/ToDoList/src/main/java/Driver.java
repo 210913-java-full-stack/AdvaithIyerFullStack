e91dbaf50748d76bdb6f966fa568025fa92aad05
advaithiyer99 <advaithiyer99@yahoo.com>
2021-09-21 15:01:00 -0700

    Committing SQL Assignment

---
+++ IdeaProjects/ToDoList/src/main/java/Driver.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import models.ToDoItem;
+
+public class ToDoDriver {
+    //Steps to creating To DoList
+    //1. Create Data
+    //2. Mark Complete
+    //3. Add items
+    //4. Add due dates
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ToDoItem> ToDoList = new ArrayList<>();
+
+
+    }
+}

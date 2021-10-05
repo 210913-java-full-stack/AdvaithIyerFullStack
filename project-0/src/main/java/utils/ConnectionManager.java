818ff6225176022f8b0070821da2c9a20358b0aa
advaithiyer99 <advaithiyer99@yahoo.com>
2021-10-05 10:00:55 -0700

    more changes

--- project-0/src/main/java/utils/ConnectionManager.java
+++ project-0/src/main/java/utils/ConnectionManager.java
@@ -7,6 +7,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+/**
+ * This file connects to the database and utilizes the connection.properties file
+ * This is a basis for inserting and connecting data to the program
+ */
+
 public class ConnectionManager {
     private static Connection conn;
 
